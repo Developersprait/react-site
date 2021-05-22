242ba4f4bfc9fff36310d3af5b3e90d56091c336
Leon Antoshyn <Doctorsprait@gmail.com>
2021-05-22 18:42:25 +0300

    about added

---
+++ src/components/About.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="about">
+            <div className="container">
+                <div className="row">
+                    <div className="col-3">
+                        <h4 className="about__h4">Who we are?</h4>
+                    </div>
+                    <div className="col-9">
+                        <div className="about__text">
+                        <p>We are a small group of people united by a common desire for beauty and development.
+                         The web is not just a word for us. 
+                         This is the whole world for us.
+                          Our association includes designers,
+                           front-end and back-end developers, 
+                         SEO and SMM specialists.
+                         And a gray cat.
+                         Our community is young, but this gives us an advantage in young professionals, 
+                         an uncomplicated view of the world and
+                          modern concepts and a craving for everything new.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
