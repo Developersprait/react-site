aed810089615d8f4aea08a10dcf169b928859a1e
Leon Antoshyn <Doctorsprait@gmail.com>
2021-05-29 16:56:02 +0300

    начата новая секция

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './sass/main.scss';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import About from './components/About';
+import Showcase from './components/Showcase';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Header />
       <About />
+      <Showcase />
     </div>
   );
 }
