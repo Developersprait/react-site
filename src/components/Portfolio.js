fc23a86ce51322fafcd2cce2dcc63094357f8454
Leon Antoshyn <Doctorsprait@gmail.com>
2021-06-02 16:18:36 +0300

    training project finished

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import cat1 from '../../src/static/img/1.png'
 import cat2 from '../../src/static/img/2.png'
-import cat3 from '../../src/static/img/3.png'
-import cat4 from '../../src/static/img/4.png'
+import cat3 from '../../src/static/img/4.png'
 
 const Portfolio = () => {
+    const [state] = React.useState([
+        {id:1, image: cat1, name:'Great Gatsby', description:'Shy cat'},
+        {id:2, image: cat2, name:'Big Brother', description:'The biggest of us'},
+        {id:3, image: cat3, name:'Gorgeous Mark', description:'Project Manager'}
+    ]);
     return (
         <div className="portfolio">
             <div className="container">
@@ -18,34 +22,21 @@ const Portfolio = () => {
                  </div>
                  <div className="col-8">
                      <div className="row">
-                         <div className="col-4 pl-15">
+                         {state.map(user =>(
+                             <div className="col-4 pl-15" key={user.id}>
                              <div className="portfolio__card">
                                  <div className="portfolio__card-img">
-                                     <img src={cat1} alt="card"/>
+                                     <img src={user.image} alt="card"/>
                                  </div>
+                                 <div className="portfolio__card-name">
+                                     {user.name}
+                                 </div>
+                                 <div className="portfolio__card-description"><p >
+                                      {user.description}</p></div>
                              </div>
                          </div>
-                         <div className="col-4 pl-15">
-                             <div className="portfolio__card">
-                                 <div className="portfolio__card-img">
-                                     <img src={cat2} alt="card"/>
-                                 </div>
-                             </div>
-                         </div>
-                         <div className="col-4 pl-15">
-                             <div className="portfolio__card">
-                                 <div className="portfolio__card-img">
-                                     <img src={cat3} alt="card"/>
-                                 </div>
-                             </div>
-                         </div>
-                         {/* <div className="col-4 pl-15">
-                             <div className="portfolio__card">
-                                 <div className="portfolio__card-img">
-                                     <img src={cat4} alt="card"/>
-                                 </div>
-                             </div>
-                         </div> */}
+                         ))}
+                         
                      </div>
                  </div>
                 </div>
